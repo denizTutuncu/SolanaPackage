d602dacb909f3e647b1f3b120644b45de4c44160
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2022-05-31 16:21:49 -0700

    Adds `BalanceLoadingViewModel`

---
+++ SolanaiOS/SolanaPackage/SolanaPackage/Solana Presantation/Balance Presentation/BalanceLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  BalanceLoadingViewModel.swift
+//  SolanaPackage
+//
+//  Created by Deniz Tutuncu on 5/31/22.
+//
+
+import Foundation
+
+public struct BalanceLoadingViewModel {
+    public let isLoading: Bool
+}
