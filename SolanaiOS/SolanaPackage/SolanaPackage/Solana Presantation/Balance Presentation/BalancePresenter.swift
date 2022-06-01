d04f52347dd5953a985a83feb0f672073ef5b1dd
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2022-05-31 17:01:19 -0700

    Adds `BalancePresenter`

---
+++ SolanaiOS/SolanaPackage/SolanaPackage/Solana Presantation/Balance Presentation/BalancePresenter.swift
@@ -0,0 +1,58 @@
+//
+//  BalancePresenter.swift
+//  SolanaPackage
+//
+//  Created by Deniz Tutuncu on 5/31/22.
+//
+
+import Foundation
+
+public protocol BalanceView {
+    func display(_ viewModel: BalanceViewModel)
+}
+
+public protocol BalanceLoadingView {
+    func display(_ viewModel: BalanceLoadingViewModel)
+}
+
+public protocol BalanceErrorView {
+    func display(_ viewModel: BalanceErrorViewModel)
+}
+
+public final class BalancePresenter {
+    private let balanceView: BalanceView
+    private let loadingView: BalanceLoadingView
+    private let errorView: BalanceErrorView
+    
+    private var balancceLoadError: String {
+        return NSLocalizedString("BALANCE_VIEW_CONNECTION_ERROR",
+                                 tableName: "Balance",
+                                 bundle: Bundle(for: BalancePresenter.self),
+                                 comment: "Error message displayed when we can't load the image feed from the server")
+    }
+    
+    public init(balanceView: BalanceView, loadingView: BalanceLoadingView, errorView: BalanceErrorView) {
+        self.balanceView = balanceView
+        self.loadingView = loadingView
+        self.errorView = errorView
+    }
+    
+    public static var title: String {
+        return NSLocalizedString("BALANCE_VIEW_TITLE", tableName: "Balance", bundle: Bundle(for: BalancePresenter.self), comment: "Title for the balance view")
+    }
+    
+    public func didStartLoadingBalance() {
+        errorView.display(.noError)
+        loadingView.display(BalanceLoadingViewModel(isLoading: true))
+    }
+    
+    public func didFinishLoadingBalance(with balance: BalanceResponse) {
+        balanceView.display(BalanceViewModel(balance: balance))
+        loadingView.display(BalanceLoadingViewModel(isLoading: false))
+    }
+    
+    public func didFinishLoadingBalance(with error: Error) {
+        errorView.display(.error(message: balancceLoadError))
+        loadingView.display(BalanceLoadingViewModel(isLoading: false))
+    }
+}
