96effd5427dff04cf794d69e757fdc9bb899ab51
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2022-05-31 23:49:06 -0700

    Adds `LoadResourcePresenterTests` for creating generic loader prensenter

---
+++ SolanaiOS/SolanaPackage/SolanaPackageTests/Balance Presentation/LoadResourcePresenterTests.swift
@@ -0,0 +1,12 @@
+//
+//  LoadResourcePresenterTests.swift
+//  SolanaPackageTests
+//
+//  Created by Deniz Tutuncu on 5/31/22.
+//
+
+import XCTest
+
+class LoadResourcePresenterTests: XCTestCase {
+    
+}
