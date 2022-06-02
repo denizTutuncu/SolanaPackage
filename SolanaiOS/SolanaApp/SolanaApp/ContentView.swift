587ae7c3ee8d9baca2b784085fd3928719964c10
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2022-06-01 17:23:38 -0700

    Adds empty SolanaApp project

---
+++ SolanaiOS/SolanaApp/SolanaApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SolanaApp
+//
+//  Created by Deniz Tutuncu on 6/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SolanaiOS/SolanaApp/SolanaApp/SolanaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolanaAppApp.swift
+//  SolanaApp
+//
+//  Created by Deniz Tutuncu on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SolanaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
