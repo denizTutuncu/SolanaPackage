66f1a4629aa56e55af17f96878212ed9f14b581c
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2022-07-01 14:40:50 -0700

    Updates `Balance` model

--- SolanaiOS/SolanaPackage/SolanaPackageUI/Balance UI/Views/BalanceView.swift
+++ SolanaiOS/SolanaPackage/SolanaPackageUI/Balance UI/Views/BalanceView.swift
@@ -38,6 +38,6 @@ struct BalanceView: View {
 
 struct SolBalanceView_Previews: PreviewProvider {
     static var previews: some View {
-        BalanceView(balance: BalanceViewModel(balance: Balance(value: 25000000000)))
+        BalanceView(balance: BalanceViewModel(balance: Balance(lamports: 25000000000)))
     }
 }
