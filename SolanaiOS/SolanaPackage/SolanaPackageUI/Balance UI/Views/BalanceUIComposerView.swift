133efe300f398fd5a39a20c2966d1c9cb3ac5983
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2022-07-01 03:42:26 -0700

    Adds `BalanceUIComposerView`

---
+++ SolanaiOS/SolanaPackage/SolanaPackageUI/Balance UI/Views/BalanceUIComposerView.swift
@@ -0,0 +1,20 @@
+//
+//  BalanceUIComposerView.swift
+//  SolanaPackageUI
+//
+//  Created by Deniz Tutuncu on 7/1/22.
+//
+
+import SwiftUI
+
+struct BalanceUIComposerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BalanceUIComposerView_Previews: PreviewProvider {
+    static var previews: some View {
+        BalanceUIComposerView()
+    }
+}
