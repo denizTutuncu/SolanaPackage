2878148191c8b1e10f6a3f265471fb1fe0ecf3d1
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2022-07-01 03:14:08 -0700

    Adds `BalanceUITests`

---
+++ SolanaiOS/SolanaPackage/SolanaPackageUITests/BalanceUITests.swift
@@ -0,0 +1,15 @@
+//
+//  BalanceUITests.swift
+//  SolanaPackageUITests
+//
+//  Created by Deniz Tutuncu on 7/1/22.
+//
+
+import XCTest
+
+class BalanceUITests: XCTestCase {
+
+    func test_assertFalse() {
+        XCTAssertFalse(false)
+    }
+}
