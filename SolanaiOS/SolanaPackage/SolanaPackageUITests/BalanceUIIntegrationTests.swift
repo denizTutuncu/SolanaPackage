4f2d015b0c80b4c558bb69b203e52a87621f897a
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2022-07-01 23:08:15 -0700

    Removes bad code.

--- SolanaiOS/SolanaPackage/SolanaPackageUITests/BalanceUIIntegrationTests.swift
+++ SolanaiOS/SolanaPackage/SolanaPackageUITests/BalanceUIIntegrationTests.swift
@@ -22,12 +22,6 @@ class BalanceUIIntegrationTests: XCTestCase {
         XCTAssertNotNil(sut.body)
     }
     
-    func test_init_SUTPublisherNotNil() {
-        let sut = makeSUT()
-        XCTAssertNotNil(sut.view)
-    }
-    
-    
     private func makeSUT() -> BalanceComposerView {
         let viewModel = BalanceViewModel(remoteBalanceLoader: RemoteBalanceLoader(url: anyURL(), methodName: anyMethod(), publicKey: anyKey(), client: URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))))
         let sut = BalanceComposerView(viewModel: viewModel)
