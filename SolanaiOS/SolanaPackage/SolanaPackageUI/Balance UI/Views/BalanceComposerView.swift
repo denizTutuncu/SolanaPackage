430a15b896e64e077affdca7bc2651687658b32f
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2022-07-05 00:39:48 -0700

    Update `BalanceView` with a Spacer

--- SolanaiOS/SolanaPackage/SolanaPackageUI/Balance UI/Views/BalanceComposerView.swift
+++ SolanaiOS/SolanaPackage/SolanaPackageUI/Balance UI/Views/BalanceComposerView.swift
@@ -32,7 +32,7 @@ public struct BalanceComposerView: View {
                 .bold()
                 .padding()
                 .foregroundColor(Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)))
-                
+                Spacer()
             viewModel.uiModel.isLoading ? AnyView(self.loadingView) : (viewModel.uiModel.error != nil) ? AnyView(self.errorView) : AnyView(self.balanceView)
         }
         .onAppear(perform: {
