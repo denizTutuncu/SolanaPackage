742b34ed4de13e3b49ab6c11d9f7f57609016982
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2023-01-11 01:45:55 -0800

    Adds test_balance_endpointURLAfterGivenWalletAddress test to `BalanceEndpointTests`

--- SolanaiOS/SolanaPackage/SolanaPackageTests/Balance API/BalanceEndpointTests.swift
+++ SolanaiOS/SolanaPackage/SolanaPackageTests/Balance API/BalanceEndpointTests.swift
@@ -8,6 +8,15 @@
 import XCTest
 import SolanaPackage
 
-class BalanceEndpoint: XCTestCase {
-    
+class BalanceEndpointTests: XCTestCase {
+    func test_balance_endpointURLAfterGivenWalletAddress() {
+        let baseURL = URL(string: "http://base-url.com")!
+        let walletAddress = "AWalletAddressAsString"
+        
+        let received = try! BalanceEndpoint.get(walletAddress: walletAddress).url(baseURL: baseURL)
+        
+        XCTAssertEqual(received.url?.scheme, "http", "scheme")
+        XCTAssertEqual(received.url?.host, "base-url.com", "host")
+        XCTAssertEqual(received.url?.absoluteString, "http://base-url.com", "absoluteString")
+    }
 }
