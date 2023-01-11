21670608be6f1b176a59e7d45382d55a282a062c
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2023-01-11 02:17:43 -0800

    Updates `BalanceViewModel` for the presentation

--- SolanaiOS/SolanaPackage/SolanaPackage/Balance Presentation/BalanceViewModel.swift
+++ SolanaiOS/SolanaPackage/SolanaPackage/Balance Presentation/BalanceViewModel.swift
@@ -1,55 +1,12 @@
-////
-////  BalanceViewModel.swift
-////  SolanaPackage
-////
-////  Created by Deniz Tutuncu on 5/31/22.
-////
 //
-//import Foundation
-//import Combine
-//import SwiftUI
+//  BalanceViewModel.swift
+//  SolanaPackage
 //
-//public class BalanceViewModel: ObservableObject {
-//    
-//    public struct BalanceUIModel {
-//        public let balance: Balance?
-//        public let error: Error?
-//        public var isLoading: Bool
-//        init(balance: Balance?, error: Error?, isLoading: Bool = true) {
-//            self.balance = balance
-//            self.error = error
-//            self.isLoading = isLoading
-//        }
-//    }
-//    
-////    public let labelTitle = NSLocalizedString("BALANCE_LABEL_TITLE", tableName: "Balance", bundle: Bundle(for: BalanceViewModel.self), comment: "Title for Label")
-//    public let loadingTitle = NSLocalizedString("BALANCE_LOADING_TITLE", tableName: "Balance", bundle: Bundle(for: BalanceViewModel.self), comment: "Title for Loading View")
-//    
-//    @Published public var uiModel: BalanceUIModel = BalanceUIModel(balance: nil, error: nil, isLoading: true)
-//    
-//    private let remoteBalanceLoader: RemoteBalanceLoader
-//    
-//    public init(remoteBalanceLoader: RemoteBalanceLoader) {
-//        self.remoteBalanceLoader = remoteBalanceLoader
-//    }
-//    
-//    public func loadBalance() {        
-//        remoteBalanceLoader.load { [weak self] result in
-//            switch result {
-//            case let .success(balance):
-//                let balanceViewModel = BalanceUIModel(balance: balance, error: nil, isLoading: false)
-//                DispatchQueue.main.async {
-//                    self?.uiModel = balanceViewModel
-//                }
-//               
-//            case let .failure(error):
-//                print(error)
-//                let balanceViewModel = BalanceUIModel(balance: nil, error: error, isLoading: false)
-//                DispatchQueue.main.async {
-//                    self?.uiModel = balanceViewModel
-//                }
-//            }
-//          
-//        }
-//    }
-//}
+//  Created by Deniz Tutuncu on 5/31/22.
+//
+
+import Foundation
+
+public struct BalanceViewModel {
+    public let balance: String?
+}
