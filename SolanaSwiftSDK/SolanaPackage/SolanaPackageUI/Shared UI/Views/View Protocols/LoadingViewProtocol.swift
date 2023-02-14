6c4eeb9d30f6ccc4d6f82b2d8ba5ad197d9d9bd7
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2023-02-13 16:54:44 -0800

    Update ResultDisplayableView and extract View Protocols to their own files.

---
+++ SolanaSwiftSDK/SolanaPackage/SolanaPackageUI/Shared UI/Views/View Protocols/LoadingViewProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  LoadingViewProtocol.swift
+//  SolanaPackageUI
+//
+//  Created by Deniz Tutuncu on 2/13/23.
+//
+
+import SwiftUI
+
+public protocol LoadingViewProtocol: View {
+    var title: String { get }
+}
