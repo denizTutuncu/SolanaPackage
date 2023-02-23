1abc307e4cf4391ef669d622c7070c97dc6ab4ea
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2023-02-23 02:27:19 -0800

    Update BalanceView via removing the state.

--- SolanaSwiftSDK/SolanaPackage/SolanaPackageUI/Balance UI/BalanceView.swift
+++ SolanaSwiftSDK/SolanaPackage/SolanaPackageUI/Balance UI/BalanceView.swift
@@ -7,9 +7,9 @@
 import SwiftUI
 
 public struct BalanceView: View {
-    @State private var title: String
-    @State private var amount: String
-    @State private var currencyName: String
+    private let title: String
+    private let amount: String
+    private let currencyName: String
     
     public init(title: String, amount: String, currencyName: String) {
         self.title = title
@@ -18,31 +18,36 @@ public struct BalanceView: View {
     }
     
     public var body: some View {
-        HStack {
-            Text("Trkigwe87343hqhdfhuqw812332rb213gasdsa")
-            Text(title)
-                .lineLimit(1)
-                .shadow(color: .primary, radius: 0.5)
-                .padding()
-                .foregroundColor(Color.primary)
-            
-            Text("\(amount) \(currencyName)")
-                .lineLimit(1)
-                .shadow(color: .primary, radius: 0.5)
-                .padding()
-                .foregroundColor(Color.primary)
-            
-            Spacer()
+        VStack {
+            HStack {
+                Text(title)
+                    .font(.system(size: 28, weight: .thin))
+                    .minimumScaleFactor(0.5)
+                    .lineLimit(1)
+                    .foregroundColor(Color.primary)
+                    .shadow(color: .primary, radius: 0.5)
+                   
+                
+                Text("\(amount) \(currencyName)")
+                    .font(.system(size: 24, weight: .light))
+                    .minimumScaleFactor(0.5)
+                    .lineLimit(1)
+                    .foregroundColor(Color.primary)
+                    .shadow(color: .primary, radius: 0.5)
+                   
+                Spacer()
+            } .padding()
         }
     }
 }
 
-struct BalanceView_Previews: PreviewProvider {
+
+struct BalanceTestView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            BalanceView(title: "Balance", amount: "10000000000", currencyName: "lamports")
+            BalanceView(title: "Balance", amount: "\(10000000000.0)", currencyName: "lamports")
                 .previewLayout(.sizeThatFits)
-                .previewDisplayName("Balance View")
+                .previewDisplayName("Balance Test View")
         }
     }
 }
