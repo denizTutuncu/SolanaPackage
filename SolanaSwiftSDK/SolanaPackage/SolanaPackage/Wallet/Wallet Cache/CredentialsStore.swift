df537da6e011aeffc76dc8146954b5e7e03eec28
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2023-04-05 14:42:57 -0700

    Move public typealias into protocol because they are closely related.

--- SolanaSwiftSDK/SolanaPackage/SolanaPackage/Wallet/Wallet Cache/CredentialsStore.swift
+++ SolanaSwiftSDK/SolanaPackage/SolanaPackage/Wallet/Wallet Cache/CredentialsStore.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 
-public typealias PublicKey = String
-public typealias PrivateKey = String
-
 public protocol CredentialsStore {
+    typealias PublicKey = String
+    typealias PrivateKey = String
+
     func insert(publicKey: PublicKey, privateKey: PrivateKey) throws
     func privateKey(for publicKey: PublicKey) throws -> PrivateKey
     func deletePrivateKey(for publicKey: PublicKey) throws
