86997d17543df46c48484919a898a33c94563b6a
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2023-04-05 00:25:45 -0700

    Adds `SolanaPackageCacheIntegrationTests` target to separate the potentially-slower cache integration tests from the fast unit/isolated test

---
+++ SolanaSwiftSDK/SolanaPackage/SolanaPackageCacheIntegrationTests/SolanaPackageCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  SolanaPackageCacheIntegrationTests.swift
+//  SolanaPackageCacheIntegrationTests
+//
+//  Created by Deniz Tutuncu on 4/5/23.
+//
+
+import XCTest
+
+final class SolanaPackageCacheIntegrationTests: XCTestCase {
+
+
+}
