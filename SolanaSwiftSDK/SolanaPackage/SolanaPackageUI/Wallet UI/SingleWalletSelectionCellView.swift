635f413da04806dc977a56e820c6d0d623120e61
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2023-04-14 14:23:27 -0700

    Organize code.

--- SolanaSwiftSDK/SolanaPackage/SolanaPackageUI/Wallet UI/SingleWalletSelectionCellView.swift
+++ SolanaSwiftSDK/SolanaPackage/SolanaPackageUI/Wallet UI/SingleWalletSelectionCellView.swift
@@ -13,25 +13,25 @@ struct SingleWalletSelectionCellView: View {
     
     var body: some View {
         Button(action: selection,
-                   label: {
-                HStack {
-                    Rectangle()
-                        .stroke(Color.secondary, lineWidth: 2.5)
-                        .overlay(
-                            Rectangle()
-                                .fill(wallet.isSelected ? Color.blue : .clear)
-                                .frame(width: 9.0, height: 9.0)
-                        )
-                        .frame(width: 13.0, height: 13.0)
-                    
-                        Text(wallet.id)
-                            .font(.system(size: 36, weight: .black, design: .monospaced))
-                            .lineLimit(1)
-                            .minimumScaleFactor(0.2)
-                            .foregroundColor(Color.primary)
-                }
-            }).padding()
-    
+               label: {
+            HStack {
+                Rectangle()
+                    .stroke(Color.secondary, lineWidth: 2.5)
+                    .overlay(
+                        Rectangle()
+                            .fill(wallet.isSelected ? Color.blue : .clear)
+                            .frame(width: 9.0, height: 9.0)
+                    )
+                    .frame(width: 13.0, height: 13.0)
+                
+                Text(wallet.id)
+                    .font(.system(size: 36, weight: .black, design: .monospaced))
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.2)
+                    .foregroundColor(Color.primary)
+            }
+        }).padding()
+        
     }
 }
 
@@ -39,9 +39,10 @@ struct SingleWalletSelectionCell_Previews: PreviewProvider {
     @State var selection: String = "none"
     
     static var previews: some View {
-        SingleWalletSelectionCellView(wallet: .constant(WalletUI(id: "4nNfoAztZVjRLLcxgcxT7yYUuyn6UgMJdduART94TrKi",
-                                                                 balance: "100.0")),
-                                      selection: { })
+        SingleWalletSelectionCellView(wallet: .constant(WalletUI(
+            id: "4nNfoAztZVjRLLcxgcxT7yYUuyn6UgMJdduART94TrKi",
+            balance: "100.0")),
+            selection: { })
         .previewLayout(.sizeThatFits)
         .previewDisplayName("Single Wallet Selection Cell Test View")
     }
