19d0dd9d3cbf7538dcc6ad620908f5ae704ee2c8
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2024-01-22 13:54:58 -0800

    - Removes comments from CustomImageView

--- SolanaSwiftSDK/SolanaPackage/SolanaPackageUI/Shared UI/CustomImageView.swift
+++ SolanaSwiftSDK/SolanaPackage/SolanaPackageUI/Shared UI/CustomImageView.swift
@@ -10,25 +10,24 @@ import SwiftUI
 import SwiftUI
 
 struct CustomImageView: View {
-    init(imageName: String, bundle: String) {
+    init(imageName: String, bundleName: String) {
         self.imageName = imageName
-        self.bundle = bundle
+        self.bundleName = bundleName
     }
     private let imageName: String
-    private let bundle: String
+    private let bundleName: String
     var body: some View {
         VStack {
-            // Replace "your_image_name" with the name of your image file in the bundle
             Image(imageName,
-                  bundle: Bundle(identifier: bundle))
-                .resizable() // Makes the image resizable
-                .aspectRatio(contentMode: .fit) // Keeps the aspect ratio and fits the content in the available space
-                .padding() // Adds some padding around the image
+                  bundle: Bundle(identifier: bundleName))
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .padding()
         }
     }
 }
 
 #Preview {
     CustomImageView(imageName:"CreationOptionsBackground",
-                    bundle: "com.deniztutuncu.SolanaPackageUI")
+                    bundleName: "com.deniztutuncu.SolanaPackageUI")
 }
