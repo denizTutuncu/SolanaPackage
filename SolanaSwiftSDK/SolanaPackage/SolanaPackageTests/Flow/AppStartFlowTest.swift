1870f4040e9632012e43d2b555a78b33a998898c
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2024-02-06 14:45:15 -0800

    - Updates tests for updated SeedDelegate and Public Key Delegate protocols - Acces providedSeed in the closure of ProvidedSeedComposerView viewModel handler

--- SolanaSwiftSDK/SolanaPackage/SolanaPackageTests/Flow/AppStartFlowTest.swift
+++ SolanaSwiftSDK/SolanaPackage/SolanaPackageTests/Flow/AppStartFlowTest.swift
@@ -14,49 +14,50 @@ class AppStartFlowTest: XCTestCase {
     func test_start_withNoWalletsAndNoSeed_doesNotDelegateWalletOrSeedHandling() {
         makeSUT(publickeys: [], seed: []).start()
         
-        XCTAssertTrue(delegate.passedPublicKeys.isEmpty)
-        XCTAssertTrue(delegate.passedSeed.isEmpty)
+        XCTAssertTrue(pkDelegate.passedPublicKeys.isEmpty)
+        XCTAssertTrue(sDelegate.passedSeed.isEmpty)
     }
     
     func test_start_withSeed_AndNoWallets_delegatesCorrectSeedHandling() {
         let localSeedPhrase = seedPhrase()
         makeSUT(publickeys: [], seed: localSeedPhrase).start()
         
-        XCTAssertEqual(delegate.passedSeed, localSeedPhrase)
+        XCTAssertEqual(sDelegate.passedSeed, localSeedPhrase)
         
-        XCTAssertEqual(delegate.passedSeed.count, 24, "Delegated seed phrase count must be 24.")
+        XCTAssertEqual(sDelegate.passedSeed.count, 24, "Delegated seed phrase count must be 24.")
     }
     
     func test_start_withAtLeastOneWalletAndNoSeed_delegatesCorrectWalletHandling() {
         makeSUT(publickeys: ["First Wallet"], seed: []).start()
         
-        XCTAssertEqual(delegate.passedPublicKeys, ["First Wallet"])
-        XCTAssertTrue(delegate.passedSeed.isEmpty)
+        XCTAssertEqual(pkDelegate.passedPublicKeys, ["First Wallet"])
+        XCTAssertTrue(sDelegate.passedSeed.isEmpty)
     }
     
     func test_start_withTwoWallets_WITHNoSeed_delegatesWalletHandling() {
         makeSUT(publickeys: ["First Wallet", "Second Wallet"], seed: []).start()
         
-        XCTAssertEqual(delegate.passedPublicKeys, ["First Wallet", "Second Wallet"])
-        XCTAssertTrue(delegate.passedSeed.isEmpty)
+        XCTAssertEqual(pkDelegate.passedPublicKeys, ["First Wallet", "Second Wallet"])
+        XCTAssertTrue(sDelegate.passedSeed.isEmpty)
     }
     
     func test_startTwice_withTwoWallets_delegatesFirstWalletHandlingTwice() {
         let sut = makeSUT(publickeys: ["First Wallet", "Second Wallet"], seed:  seedPhrase())
         
         sut.start()
-        XCTAssertEqual(delegate.passedPublicKeys, ["First Wallet", "Second Wallet"])
-        XCTAssertEqual(delegate.passedSeed, [])
+        XCTAssertEqual(pkDelegate.passedPublicKeys, ["First Wallet", "Second Wallet"])
+        XCTAssertEqual(sDelegate.passedSeed, [])
 
 
         sut.start()
-        XCTAssertEqual(delegate.passedPublicKeys, ["First Wallet", "Second Wallet"])
-        XCTAssertEqual(delegate.passedSeed, [])
+        XCTAssertEqual(pkDelegate.passedPublicKeys, ["First Wallet", "Second Wallet"])
+        XCTAssertEqual(sDelegate.passedSeed, [])
     }
     
     // MARK: Helpers
-    private let delegate = PublicKeyDelegateSpy()
-    private weak var weakSUT: AppStartFlow<PublicKeyDelegateSpy>?
+    private let pkDelegate = PublicKeyDelegateSpy()
+    private let sDelegate = SeedDelegateSpy()
+    private weak var weakSUT: AppStartFlow<PublicKeyDelegateSpy, SeedDelegateSpy>?
     
     override func tearDown() {
         super.tearDown()
@@ -64,8 +65,8 @@ class AppStartFlowTest: XCTestCase {
         XCTAssertNil(weakSUT, "Memory leak detected. Weak reference to the SUT instance is not nil.")
     }
     
-    private func makeSUT(publickeys: [String], seed: [String]) -> AppStartFlow<PublicKeyDelegateSpy> {
-        let sut = AppStartFlow(publickeys: publickeys, seed: seed, delegate: delegate)
+    private func makeSUT(publickeys: [String], seed: [String]) -> AppStartFlow<PublicKeyDelegateSpy, SeedDelegateSpy> {
+        let sut = AppStartFlow(publickeys: publickeys, seed: seed, pkDelegate: pkDelegate, sDelegate: sDelegate)
         weakSUT = sut
         return sut
     }
